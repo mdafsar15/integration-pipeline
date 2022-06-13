c9c428afb29c05b169556981a6319c14a02803e2
afsar.ansari <afsaralam7200@gmail.com>
2022-06-13 23:19:27 +0530

    demo

--- demo.sh
+++ demo.sh
@@ -1,3 +1,1 @@
-echo "************** demo start ********************";
-
-sh 'sudo sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no oracle@192.168.0.93'
+sudo sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no oracle@192.168.0.93
--- demo.sh.bak
+++ demo.sh.bak
@@ -1,3 +1,3 @@
 echo "************** demo start ********************";
 
-sh 'sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no oracle@192.168.0.93'
+sh 'sudo sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no oracle@192.168.0.93'
